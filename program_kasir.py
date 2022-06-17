f61cdc4e6773e1ee4c849ca77b2992d354f73b75
Alia Prabandari <aliaprabandari@student.uns.ac.id>
2022-06-17 08:20:56 +0700

    fungsi program kasir

--- program_kasir.py
+++ program_kasir.py
@@ -2,7 +2,7 @@ import csv
 import os
 from fungsi_pembelian import *
 
-
+#mencetak daftar menu dari csv
 def menu():
     print("="*61)
     print('{:^65}'.format("         -WELCOME TO SUNSET RESTORAN-            "))
@@ -11,13 +11,13 @@ def menu():
 
     
 
-    with open('menu.csv', newline='') as f:
+    with open('menu.csv', newline='') as f: #membuka csv
         reader = csv.reader(f)
         data = list(reader)
 
 
 
-    for x in range(len(data)):
+    for x in range(len(data)): #mencetak data dari csv
         print(data[x][0],". " ,data[x][1],"= Rp.",data[x][2])
 
 
